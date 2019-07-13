6cfe4beeb5849abd76e9a477faf13237776106d5
mamitianaignace <mamitianaignace@gmail.com>
2019-07-13 19:56:25 +0300

    style steps

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PresentationalComponent from './components/PresentationalComponent'
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
 
-
-export default class App extends React.Component {
-
-  state = {
-    myState1: 'my state one , lorem ipsum'
-  }
-
-  updateState = () => this.setState({myState1 : 'niova ny states'})
-   render() {
-      return (
-        <View>
-        <PresentationalComponent myState1 = {this.state.myState1}  updateState = {this.updateState} />
-        </View>
-      );
-   }
+const App = (props) => {
+   return (
+      <View style = {styles.container}>
+         <View style = {styles.redbox} />
+         <View style = {styles.bluebox} />
+         <View style = {styles.blackbox} />
+      </View>
+   )
 }
 
+export default App
 
+const styles = StyleSheet.create ({
+   container: {
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'flex-end',
+      backgroundColor: 'grey',
+      height: 600
+   },
+   redbox: {
+      width: 100,
+      height: 100,
+      backgroundColor: 'red'
+   },
+   bluebox: {
+      width: 100,
+      height: 100,
+      backgroundColor: 'blue'
+   },
+   blackbox: {
+      width: 100,
+      height: 100,
+      backgroundColor: 'black'
+   },
+})
