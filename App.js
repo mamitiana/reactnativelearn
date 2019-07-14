6c4a515298de48f4e3c1af863bbb9adc9aea14ab
mamitianaignace <mamitianaignace@gmail.com>
2019-07-14 13:07:23 +0300

    button steps

--- App.js
+++ App.js
@@ -3,10 +3,32 @@ import List from './components/List'
 import Inputs from './components/Input'
 import ScrollViewExample from './components/ScrollView'
 import HttpExample from './components/http_exemple'
+import { Button } from 'react-native'
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native'
+
 
 const App = () => {
    return (
-      <HttpExample />
+      <View style = {styles.container}>
+         <TouchableOpacity>
+            <Text style = {styles.text}>
+               Button
+            </Text>
+         </TouchableOpacity>
+      </View>
    )
 }
 export default App
+
+const styles = StyleSheet.create ({
+   container: {
+      alignItems: 'center',
+   },
+   text: {
+      borderWidth: 1,
+      padding: 25,
+      borderColor: 'black',
+      backgroundColor: 'red'
+   }
+})
+//export default App
