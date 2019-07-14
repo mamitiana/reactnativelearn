5c3554d0a387f12787320cfd6edf167a749bc103
mamitianaignace <mamitianaignace@gmail.com>
2019-07-14 12:30:23 +0300

    http step

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import List from './components/List'
 import Inputs from './components/Input'
 import ScrollViewExample from './components/ScrollView'
+import HttpExample from './components/http_exemple'
+
 const App = () => {
    return (
-      <ScrollViewExample />
+      <HttpExample />
    )
 }
 export default App
